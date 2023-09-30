e8d36142390f5b35fc0202d869303b83f63181b5
Bill <bill@thewallisfamily.co.uk>
2023-09-30 15:43:18 +0100

    ✅ test: Add more unit tests

--- tests/unit/test__query_timer.py
+++ tests/unit/test__query_timer.py
@@ -4,6 +4,7 @@ Unit tests for the ``db_query_profiler.query_timer`` module.
 import contextlib
 import io
 import re
+import sqlite3
 from pathlib import Path
 
 import pytest
@@ -29,7 +30,9 @@ def runner_2():
 
 def test__runner__repr(runner_1: query_timer.Runner):
     """
-    This should test that runner's ``__repr__`` creates an identical object,
+    Test the Runner's ``__repr__`` method.
+
+    This should test that Runner's ``__repr__`` creates an identical object,
     but passing in a lambda/partial is making this fiddly. Instead, this
     just tests the returned ``__repr__`` pattern.
     """
@@ -39,6 +42,17 @@ def test__runner__repr(runner_1: query_timer.Runner):
     actual = repr(runner_1)
 
     assert expected.match(actual)
+
+
+def test__runner__str(runner_1: query_timer.Runner):
+    """
+    Test the Runner's ``__str__`` method.
+    """
+    expected = (
+        "Runner(runner=[[OrderedDict()], <class 'inspect._empty'>], name=query-1.sql)"
+    )
+
+    assert str(runner_1) == expected
 
 
 def test__runner__call_without_timeit(runner_1: query_timer.Runner):
@@ -64,7 +78,7 @@ def test__runner__call_with_timeit(runner_1: query_timer.Runner):
 
 def test__runner__average_time(runner_1: query_timer.Runner):
     """
-    Test that the Runner's ``average_time`` time is computed correctly.
+    Test the Runner's ``average_time`` computation.
     """
     for _ in range(3):
         runner_1(time_it=True)
@@ -83,14 +97,14 @@ def test__runner__average_time(runner_1: query_timer.Runner):
 )
 def test__safe_divide(numerator: float, denominator: float, expected: float):
     """
-    Test that ``_safe_divide`` returns the correct value.
+    Test the ``_safe_divide`` function.
     """
     assert query_timer._safe_divide(numerator, denominator) == expected
 
 
-def test__get_query_filepaths(directory):
+def test__get_query_filepaths(directory: Path):
     """
-    Test that ``_get_query_filepaths`` returns the correct filepaths.
+    Test the ``_get_query_filepaths`` function.
     """
     expected = [
         directory / "query-1.sql",
@@ -101,9 +115,9 @@ def test__get_query_filepaths(directory):
     assert sorted(actual) == sorted(expected)
 
 
-def test__get_query_filepaths__with_warning(directory):
+def test__get_query_filepaths__with_warning(directory: Path):
     """
-    Test that ``_get_query_filepaths`` returns the correct filepaths.
+    Test the ``_get_query_filepaths`` function.
     """
     file_path = Path(directory / "temp.py")
     file_path.touch()
@@ -114,33 +128,40 @@ def test__get_query_filepaths__with_warning(directory):
     file_path.unlink()
 
 
-@pytest.mark.skip(reason="Currently, can't compare runner functions.")
-def test__create_query_runners(db_connection, directory):
+def test__create_query_runners(db_connection: sqlite3.Connection, directory: Path):
     """
-    Test that ``_create_query_runners`` returns the correct runners.
+    Test the ``_create_query_runners`` function.
     """
-    expected = [
-        query_timer.Runner(
-            name="query-1.sql",
-            runner=lambda: db_connection.execute(""),
-        ),
-        query_timer.Runner(
-            name="query-2.sql",
-            runner=lambda: db_connection.execute(""),
-        ),
-    ]
     actual = query_timer._create_query_runners(
-        directory=directory, db_conn=db_connection
+        directory=directory,
+        db_conn=db_connection,
     )
+    expected = [
+        f"""Runner(runner=[[OrderedDict([('f', <Parameter "f={repr(directory / 'query-1.sql')}">)])], <class 'inspect._empty'>], name=query-1.sql)""",  # noqa
+        f"""Runner(runner=[[OrderedDict([('f', <Parameter "f={repr(directory / 'query-2.sql')}">)])], <class 'inspect._empty'>], name=query-2.sql)""",  # noqa
+    ]
 
-    # The underlying functions have different memory addresses, so we can't
-    # rely on the `__repr__` that is implicitly called.
     assert [str(runner) for runner in actual] == [str(runner) for runner in expected]
 
 
-def test__print_runner_stats(directory):
+def test__run_runners(runner_1: query_timer.Runner, runner_2: query_timer.Runner):
     """
-    Test that ``_print_runner_stats`` prints the correct output.
+    Test the ``_run_runners`` function.
+    """
+    query_timer._run_runners(
+        runners=[runner_1, runner_2],
+        repeat=3,
+    )
+
+    assert runner_1.repeat == 3
+    assert runner_2.repeat == 3
+    assert runner_1.total_time > 0
+    assert runner_2.total_time > 0
+
+
+def test__print_runner_stats(directory: Path):  # noqa
+    """
+    Test the ``_print_runner_stats`` function.
     """
     expected = [
         "query-1.sql: 0.00000000s (0.0%)",
@@ -165,10 +186,18 @@ def test__print_runner_stats(directory):
     assert actual == expected
 
 
-def test__time_queries__with_error(db_connection):
+@pytest.mark.skip("Need to figure out how to unit test a decorator.")
+def test__print_times():
     """
-    Test that ``time_queries`` raises an error when the directory doesn't
-    exist.
+    Test the ``_print_times`` function.
+    """
+    pass
+
+
+def test__time_queries__with_error(db_connection: sqlite3.Connection):
+    """
+    Test that the ``time_queries`` function raises an error when the
+    directory doesn't exist.
     """
     with pytest.raises(FileNotFoundError):
         query_timer.time_queries(
